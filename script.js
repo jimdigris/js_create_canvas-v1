06515d4d144648ab66a772ebb0d70aa4bfa6f007
jimdirgris <34837142+jimdigris@users.noreply.github.com>
2022-11-29 17:19:21 +0300

    Add files via upload
    добавил создание Канвы через класс

--- script.js
+++ script.js
@@ -1,5 +1,6 @@
 'use strict';
 
+// через объект
 (function () {
     let canvas = {                                                                      // характеристики холста
         parent: '#wrap',                                                                // родительский элемент
@@ -17,9 +18,7 @@
             element.setAttribute('height', this.height);                                // задать высоту     
             parent.prepend(element);                                                    // добавить в родителя
         },
-
         receiveCanvas() { this.element = document.querySelector(`${this.id}`); },       // получить холст
-
         defineCanvas() { this.context = this.element.getContext("2d"); }                // определить контекст
     };
 
@@ -29,9 +28,49 @@
         canvas.createCanvas();                                                          // создать холст
         canvas.receiveCanvas();                                                         // получить холст
         canvas.defineCanvas()                                                           // определить контекст
-        console.log(canvas);
     });
 
     // ! -----------------------------------------------------------
+})();
+
+// через класс
+(function () {
+    class Canvas {                                                                      // класс для Холстов
+        element = null;                                                                 // холст
+        context = null;                                                                 // контекст
+
+        constructor(parent, id, width, height) {
+            this.parent = parent;                                                       // родительский элемент
+            this.id = id;                                                               // идентификатор
+            this.width = width;                                                         // ширина
+            this.height = height;                                                       // высота
+        }
+
+        createCanvas() {                                                                // создать холст
+            let parent = document.querySelector(`${this.parent}`);                      // получить родительский элемент
+            let element = document.createElement('canvas');                             // создать элемент холста
+            element.setAttribute('id', this.id);                                        // присвоить id      
+            element.setAttribute('width', this.width);                                  // задать ширину     
+            element.setAttribute('height', this.height);                                // задать высоту     
+            parent.prepend(element);                                                    // добавить в родителя
+        }
+        receiveCanvas() { this.element = document.querySelector(`${this.id}`); }        // получить холст
+        defineCanvas() { this.context = this.element.getContext("2d"); }                // определить контекст    
+
+        draw() {                                                                        // отрисовать холст
+            this.createCanvas();                                                        // создать холст
+            this.receiveCanvas();                                                       // получить холст
+            this.defineCanvas()                                                         // определить контекст
+        }
+    }
+
+    // ! -----------------------------------------------------------
+
+    document.addEventListener("DOMContentLoaded", () => {                               // загрузка страницы
+        let canva = new Canvas('#wrap', 'canvas', 600, 200);                            // создать экземпляр
+        canva.draw();                                                                   // отрисовать холст
+    });
+
+    // ! -----------------------------------------------------------    
 
 })();
